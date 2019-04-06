15de9de61e8b0db640bd550be823be5c703e6815
Rachid <ouattchidi@gmail.com>
2019-04-07 00:32:35 +0200

    Fix lint issues

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -18,11 +18,13 @@ def test_service_is_running(host):
     assert service.is_running
     assert service.is_enabled
 
+
 def test_default_inputs_contains_dummy(host):
     default_conf_file = host.file('/etc/td-agent-bit/td-agent-bit.conf')
 
     assert default_conf_file.exists
     assert default_conf_file.contains("dummy")
+
 
 def test_additional_cpu_conf_file_exist(host):
     cpu_conf_file = host.file('/etc/td-agent-bit/cpu.conf')
